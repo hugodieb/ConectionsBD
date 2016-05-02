c01dfdc8803a3e4f7e18c98ca7b113dde4b958f6
Hugo Dieb <hugodieb.hd@gmail.com>
2016-05-02 10:40:52 -0300

    commit projetinho de conexoes com banco de dados access, mysql e sqlserver.

--- Conections/Conections/main.vb
+++ Conections/Conections/main.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
+Imports System.Data.SqlClient
 
 Public Class main
 
@@ -30,8 +32,67 @@ Public Class main
         End Using
     End Sub
 
+    Private Sub loadDataMYSQL()
+        Using con As MySqlConnection = getConnectionMYSQL()
+
+            Try
+                con.Open()
+                sql = "SELECT * FROM cadastro"
+
+                Dim cm As MySqlCommand
+                Dim da As MySqlDataAdapter
+                cm = New MySqlCommand(sql, con)
+                da = New MySqlDataAdapter(cm)
+                dt = New DataTable
+
+                da.Fill(dt)
+                dgvBD.DataSource = dt
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                con.Close()
+
+            End Try
+
+        End Using
+    End Sub
+
+    Private Sub loadDataSQLServer()
+        Dim cm As SqlCommand
+        Dim da As SqlDataAdapter
+        Dim dt As DataTable
+
+        Using con As SqlConnection = getConnectionSQLServer()
+            Try
+                con.Open()
+
+                sql = "SELECT * FROM CADASTRO"
+
+                cm = New SqlCommand(sql, con)
+                da = New SqlDataAdapter(cm)
+                dt = New DataTable
+                da.Fill(dt)
+                dgvBD.DataSource = dt
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                con.Close()
+            End Try
+        End Using
+    End Sub
+
     
     Private Sub btnAccess_Click(sender As Object, e As EventArgs) Handles btnAccess.Click
         loadDataAccess()
+    End Sub
+
+    Private Sub btnMYSQL_Click(sender As Object, e As EventArgs) Handles btnMYSQL.Click
+        loadDataMYSQL()
+    End Sub
+
+    Private Sub btnSQLServer_Click(sender As Object, e As EventArgs) Handles btnSQLServer.Click
+        loadDataSQLServer()
     End Sub
 End Class
--- Conections/Conections/mdlAcesso.vb
+++ Conections/Conections/mdlAcesso.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
+Imports System.Data.SqlClient
 
 Module mdlAcesso
 
@@ -6,9 +8,22 @@ Module mdlAcesso
 
     Public Function getConnectionAccess() As OleDbConnection
 
-        sql = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=G:\projetos_vb\conectionsBD\bdAccess.accdb"
+        sql = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=G:\projetos_vb\conectionsBD\ConectionsBD\bdAccess.accdb"
 
         Return New OleDbConnection(sql)
+    End Function
+
+    Public Function getConnectionMYSQL() As MySqlConnection
+
+        sql = "SERVER=localhost; DATABASE=pessoa;Uid=root;Pwd=root"
+
+        Return New MySqlConnection(sql)
+    End Function
+
+    Public Function getConnectionSQLServer() As SqlConnection
+
+        sql = "Data Source=(LocalDB)\v11.0;AttachDbFilename=G:\projetos_vb\conectionsBD\ConectionsBD\Conections\Conections\pessoa.mdf;Integrated Security=True"
+        Return New SqlConnection(sql)
     End Function
 
 End Module
